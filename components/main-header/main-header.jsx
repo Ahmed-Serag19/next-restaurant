f9ede60efdc39389652909d38b1a750939054a7b
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2025-01-23 20:07:25 +0200

    splitted the components

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
@@ -16,10 +17,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals"> Browse Meals</Link>
+              <NavLink href="/meals" children="Meals" />
             </li>
             <li>
-              <Link href="/community"> Foodies Community</Link>
+              <NavLink href="/community" children="Foodies Community" />
             </li>
           </ul>
         </nav>
