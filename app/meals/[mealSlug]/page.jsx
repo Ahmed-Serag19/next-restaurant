6de7cc3105285d6f13b0c7b41505d4ebf463e1f0
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2025-01-27 19:54:10 +0200

    finished MealDetail page with the slug

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
-const MealDetailPage = ({ params }) => {
+const MealDetailPage = async ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image />
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
-      <main></main>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 };
